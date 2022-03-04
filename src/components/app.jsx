b25a8a35f29d5f37bce24fd34b38d67e6eb174a8
K. M. Rajib Faysal <kmrajibfaysal@gmail.com>
2022-03-04 08:32:58 +0600

    clearing files

---
+++ src/components/app.jsx
@@ -0,0 +1,14 @@
+/* eslint-disable react/prefer-stateless-function */
+import { Component } from 'react';
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Hello world~~~~~~~~~~~~~~</h1>
+            </div>
+        );
+    }
+}
+
+export default App;
