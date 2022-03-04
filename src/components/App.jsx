f97f2cf9789ca351a6a93f853f00b6133cfc1f95
K. M. Rajib Faysal <kmrajibfaysal@gmail.com>
2022-03-04 21:07:54 +0600

    added profile component

---
+++ src/components/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Profile from './profile/index';
+
+// eslint-disable-next-line react/prefer-stateless-function
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <Profile />
+            </div>
+        );
+    }
+}
+
+export default App;
---
+++ src/components/profile/index.jsx
@@ -0,0 +1,41 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+/* eslint-disable react/prefer-stateless-function */
+import React from 'react';
+import './profile.style.css';
+
+class Profile extends React.Component {
+    render() {
+        return (
+            <div className="container">
+                <div className="bio">
+                    <h3>K. M. Rajib Faysal</h3>
+                    <p>Fullstack Javascript Developer, Entrepreneur and Trainer</p>
+                </div>
+                <div className="skills">
+                    <h3>Skills:</h3>
+                    <ul>
+                        <li>Java</li>
+                        <li>Javascript</li>
+                        <li>Python</li>
+                    </ul>
+                </div>
+                <div className="links">
+                    <h3>Social Links:</h3>
+                    <ul>
+                        <li>
+                            <a href="#">Facebook</a>
+                        </li>
+                        <li>
+                            <a href="#">LinkedIn</a>
+                        </li>
+                        <li>
+                            <a href="#">Github</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Profile;
